47fcef2c2b09aa4b1efb0efe8b9e6c892a25ff38
Rootshell <bcairns@voidrealms.com>
2018-04-05 16:48:27 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  String _value = 'Hello world';
+  
+  void _onPressed() {
+    setState(() => _value = 'Bryan Cairns');
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(onPressed: _onPressed, child: new Text('Click me'),)
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
